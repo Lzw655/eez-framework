6ecff9a5c214076e4c085862ad58487952d011a5
Liu Zhongwei <liuzhongwei@espressif.com>
2024-08-20 10:56:13 +0800

    feat: support platform esp

---
+++ examples/esp_ui_phone_p4_function_ev_board/main/main.c
@@ -0,0 +1,65 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_check.h"
+#include "esp_log.h"
+#include "bsp/esp-bsp.h"
+#include "ui/ui.h"
+
+static const char *TAG = "app_main";
+
+void app_main(void)
+{
+    bsp_i2c_init();
+    bsp_display_cfg_t cfg = {
+        .lvgl_port_cfg = ESP_LVGL_PORT_INIT_CONFIG(),
+        .buffer_size = BSP_LCD_H_RES * BSP_LCD_V_RES,
+        .flags = {
+            .buff_spiram = true,
+        }
+    };
+    lv_disp_t *disp = bsp_display_start_with_config(&cfg);
+    bsp_display_backlight_on();
+
+    /**
+     * To avoid errors caused by multiple tasks simultaneously accessing LVGL,
+     * should acquire a lock before operating on LVGL.
+     */
+    bsp_display_lock(0);
+
+    ui_init();
+
+    /* Release the lock */
+    bsp_display_unlock();
+
+
+    while (1) {
+        ui_tick();
+        vTaskDelay(pdMS_TO_TICKS(1));
+    }
+
+#if EXAMPLE_SHOW_MEM_INFO
+    char buffer[128];    /* Make sure buffer is enough for `sprintf` */
+    size_t internal_free = 0;
+    size_t internal_total = 0;
+    size_t external_free = 0;
+    size_t external_total = 0;
+    while (1) {
+        internal_free = heap_caps_get_free_size(MALLOC_CAP_INTERNAL);
+        internal_total = heap_caps_get_total_size(MALLOC_CAP_INTERNAL);
+        external_free = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
+        external_total = heap_caps_get_total_size(MALLOC_CAP_SPIRAM);
+        sprintf(buffer, "   Biggest /     Free /    Total\n"
+                "\t  SRAM : [%8d / %8d / %8d]\n"
+                "\t PSRAM : [%8d / %8d / %8d]",
+                heap_caps_get_largest_free_block(MALLOC_CAP_INTERNAL), internal_free, internal_total,
+                heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM), external_free, external_total);
+        ESP_LOGI("MEM", "%s", buffer);
+    }
+#endif
+}
